f628d1594096e86e1414628eb8d52919ef69b39f
HyeonSeong <gus8131@naver.com>
2026-05-18 14:20:18 +0900

    first commit

---
+++ src/main/java/com/leets/deepjava/DeepJavaApplication.java
@@ -0,0 +1,13 @@
+package com.leets.deepjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leets/deepjava/DeepJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leets.deepjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
